8e4d67c8efe158d71724f3a5ae51e5419986e5a7
Prissview <135519937+Prissview@users.noreply.github.com>
2024-02-20 23:06:34 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mon Premier Site PHP</title>
+</head>
+<body>
+    <h1>Bienvenue sur Mon Premier Site PHP</h1>
+    <p>Ceci est une page PHP simple.</p>
+
+    <?php
+    // Ajoutez du code PHP ici
+    echo "Le serveur PHP fonctionne correctement !";
+    ?>
+</body>
+</html>
